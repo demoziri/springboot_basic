0dde7c7060cbdabbef0314fa80a46eb33876197a
demoziri <redhorangy1@naver.com>
2023-04-13 16:28:09 +0900

    게시물 컨트롤러에 게시물 추가, 리스트 기능 구현

--- src/main/java/com/pjs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/pjs/exam/demo/controller/UsrHomeController.java
@@ -6,26 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	private int count;
 	
-	public UsrHomeController() {
-		count = -1;
-	}
-	
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public int getCount() {
-		return count;
-	}
-	
-	@RequestMapping("/usr/home/doSetCount")
-	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count;
-		return "count의 값이" + this.count +"으로 초기화 되었습니다.";
+	public String showMain() {
+		
+		return "메인";
 	}
 	
 }
+
 
 
 
