8a8e712e0583852d78b90f4c23f2880262299554
demoziri <redhorangy1@naver.com>
2023-04-13 17:24:08 +0900

    게시글 수정 기능 구현

--- src/main/java/com/pjs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pjs/exam/demo/controller/UsrArticleController.java
@@ -27,13 +27,14 @@ public class UsrArticleController {
 	//서비스 메서드 시작
 	private void makeTestData() {
 		for(int i=1;i<11;i++) {
-			int id = articlesLastId ++;
+			
 			String title = "제목" + i;
 			String body = "내용" + i;
 			
 			writeArticle(title,body);
 		}
 	}
+	
 	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
@@ -51,9 +52,16 @@ public class UsrArticleController {
 		}
 		return null;
 	}
+	
 	private void deleteArticle(int id) {
 		Article article = getArticle(id);
 		articles.remove(article);
+	}
+	
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body);
 	}
 	//서비스 메서드 끝
 	
@@ -72,6 +80,7 @@ public class UsrArticleController {
 		
 		return articles;
 	}
+	
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
@@ -82,6 +91,18 @@ public class UsrArticleController {
 		
 		deleteArticle(id);
 		return id + "번 게시물 삭제되었습니다.";
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id,String title, String body) {
+		Article article = getArticle(id);
+		if(article == null) {
+			return id + "번 게시물이 존재하지 않습니다.";
+		}
+		
+		modifyArticle(id,title,body);
+		return id + "번 게시물 수정되었습니다.";
 	}
 }
 
