59abddcc178d075fc2413e739b60a4b68e935d73
demoziri <redhorangy1@naver.com>
2023-04-14 17:01:03 +0900

    test2

--- src/main/java/com/pjs/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/pjs/exam/demo/repository/ArticleRepository.java
@@ -1,63 +1,24 @@
 package com.pjs.exam.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.ibatis.annotations.Mapper;
 
 import com.pjs.exam.demo.vo.Article;
 
 
-@Component
-public class ArticleRepository {
-	private int articlesLastId;
-	private List<Article> articles;
-	
-	public ArticleRepository() {
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-	}
-	
-	public void makeTestData() {
-		for (int i = 1; i < 11; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			writeArticle(title, body);
-		}
-	}
-	
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		articlesLastId = id;
+@Mapper
+public interface ArticleRepository {
 
-		return article;
-	}
-
-	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
+	public Article getArticle(int id);
 	
-	public List<Article> getArticles() {
-		return articles;
-	}
+	public List<Article> getArticles();
 
-	public void deleteArticle(int id) {
-		Article article = getArticle(id);
-		articles.remove(article);
-	}
+	public Article writeArticle(String title, String body);
+	
+	public void deleteArticle(int id);
 
-	public void modifyArticle(int id, String title, String body) {
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body);
-	}
+	public void modifyArticle(int id, String title, String body);
 	
 	
 }
