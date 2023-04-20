184eccd525c2243476d538bd4c2ecc67e0bfcc18
demoziri <redhorangy1@naver.com>
2023-04-20 14:49:29 +0900

    게시물 상세페이지에 작성자명 표시

--- src/main/java/com/pjs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/pjs/exam/demo/controller/UsrArticleController.java
@@ -47,14 +47,14 @@ public class UsrArticleController {
 		ResultData<Integer> writeArticleRd = articleService.writeArticle(loginedMemberId, title,body);
 		int id = (int)writeArticleRd.getData1();
 		
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 		
 		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(),"article", article);
 	}
 	
 	@RequestMapping("/usr/article/list")
 	public String showList(Model model) {
-		List<Article> articles = articleService.getArticles();
+		List<Article> articles = articleService.getForPrintArticles();
 		model.addAttribute("articles",articles);
 		
 		return "usr/article/list";
@@ -62,7 +62,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(Model model, int id) {
 		
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 		model.addAttribute(article);
 		
 		return "usr/article/detail";
@@ -71,7 +71,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
 	public ResultData<Article> getArticle(int id) {
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 		
 		if(article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
@@ -94,7 +94,7 @@ public class UsrArticleController {
 			return ResultData.from("F-4", "로그인 후 이용해주세요.");
 		}
 		
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 		if(article.getMemberId() != loginedMemberId) {
 			return ResultData.from("F-2", "권한이 없습니다.");
 		}
@@ -121,7 +121,7 @@ public class UsrArticleController {
 		if(isLogined == false) {
 			return ResultData.from("F-4", "로그인 후 이용해주세요.");
 		}
-		Article article = articleService.getArticle(id);
+		Article article = articleService.getForPrintArticle(id);
 		
 		if(article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
