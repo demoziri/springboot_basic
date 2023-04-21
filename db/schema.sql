62fc388748b094e07ce9152bca54bf4a701257f5
demoziri <redhorangy1@naver.com>
2023-04-21 18:28:54 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -98,3 +98,39 @@ email = 'admin@sbs.com';
 
 SELECT * FROM MEMBER;
 
+
+
+#게시판 테이블 생성
+CREATE TABLE board(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    `code` CHAR(50) NOT NULL COMMENT 'notice(공지사항), free(자유게시판1), free(자유게시판2,...',
+    `name` CHAR(50) NOT NULL COMMENT '게시판 이름',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=탈퇴전, 1=탈퇴)',
+    delDate DATETIME COMMENT '삭제날짜'
+);
+
+#기본 게시판 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code`= 'notice',
+`name`= '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code`= 'free1',
+`name`= '자유';
+
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+SELECT * FROM article;
+
+UPDATE article
+SET boardId=1
+WHERE boardId = 0;
+
+UPDATE article
+SET boardId=2
+WHERE id IN(12,17,19);s
