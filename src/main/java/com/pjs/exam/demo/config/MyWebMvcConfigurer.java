fae02bfd81e2cb4a283586b21cf1d340647c080d
demoziri <redhorangy1@naver.com>
2023-04-24 16:56:57 +0900

    페이지 메뉴 구현

--- src/main/java/com/pjs/exam/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/pjs/exam/demo/config/MyWebMvcConfigurer.java
@@ -25,13 +25,11 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 				.excludePathPatterns("/error")
 				.excludePathPatterns("/resource/**");
 		
-		
 		registry.addInterceptor(needLoginInterceptor)
-		.addPathPatterns("/usr/article/write")
-		.addPathPatterns("/usr/article/doWrite")
-		.addPathPatterns("/usr/article/modify")
-		.addPathPatterns("/usr/article/doModify")
-		.addPathPatterns("/usr/article/doDelete");
-
+				.addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite")
+				.addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify")
+				.addPathPatterns("/usr/article/doDelete");
 	}
 }
