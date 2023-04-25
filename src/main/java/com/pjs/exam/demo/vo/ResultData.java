ce518103a70614dc1d04f2d6d07f3d4a25142fde
demoziri <redhorangy1@naver.com>
2023-04-25 17:58:38 +0900

    조회수 증가 성공에 대한 답장에, data2(글번호) 정보 추가

--- src/main/java/com/pjs/exam/demo/vo/ResultData.java
+++ src/main/java/com/pjs/exam/demo/vo/ResultData.java
@@ -12,8 +12,11 @@ public class ResultData<DT> {
 	private String data1Name;
 	@Getter
 	private DT data1;
+	@Getter
+	private String data2Name;
+	@Getter
+	private Object data2;
 	
-	private ResultData() {}
 	
 	public static ResultData from(String resultCode, String msg) {
 		return from(resultCode,msg,null,null);
@@ -39,6 +42,11 @@ public class ResultData<DT> {
 	
 	public boolean isFail() {
 		return isSuccess() == false;
+	}
+
+	public void setData2(String dataName, Object data) {
+		data2Name = dataName;
+		data2 = data;
 	}
 
 
