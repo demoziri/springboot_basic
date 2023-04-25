ca2dd07b8e2153486f6840207439534a7066a865
demoziri <redhorangy1@naver.com>
2023-04-25 18:39:16 +0900

    리액션포인트 테이블 추가

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,9 @@
-DROP DATABASE IF EXISTS springboot_basic;
-CREATE DATABASE springboot_basic;
-USE springboot_basic;
+# DB 생성
+DROP DATABASE IF EXISTS sbs_proj_2023;
+CREATE DATABASE sbs_proj_2023;
+USE sbs_proj_2023;
 
+# 게시물 테이블 생성
 CREATE TABLE article (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -10,14 +12,12 @@ CREATE TABLE article (
     `body` TEXT NOT NULL
 );
 
-DROP TABLE article
-
+# 게시물, 테스트 데이터 생성
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목1',
 `body` = '내용1';
-
 
 INSERT INTO article
 SET regDate = NOW(),
@@ -25,27 +25,23 @@ updateDate = NOW(),
 title = '제목2',
 `body` = '내용2';
 
-
 INSERT INTO article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
 
-SELECT * FROM article;
-
 #게시물 테이블에 회원정보 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
 
 # 기존 게시물의 작성자를 2번으로 지정
-UPDATE article 
-SET memberId = 2
-WHERE memberId = 0;
+UPDATE article
+SET memberId = 2;
+WHERE memberid = 0;
 
+SELECT * FROM article;
 
-
-#member
-
+# 회원 테이블 생성
 CREATE TABLE `member` (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -57,10 +53,11 @@ CREATE TABLE `member` (
     `nickname` CHAR(20) NOT NULL,
     cellphoneNo CHAR(20) NOT NULL,
     email CHAR(50) NOT NULL,
-    delSta`member`tus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '(탈퇴여부)',
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴여부',
     delDate DATETIME COMMENT '탈퇴날짜'
 );
 
+# 회원, 테스트 데이터 생성
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
@@ -69,77 +66,146 @@ loginPw = 'admin',
 authLevel = 7,
 `name` = '관리자',
 nickname = '관리자',
-cellphoneNo = '01012341234',
-email = 'admin@sbs.com';
-INSERT INTO `member`
-SET regDate = NOW(),
-updateDate = NOW(),
-loginId = 'member',
-loginPw = 'member',
-authLevel = 3,
-`name` = '회원1',
-nickname = '회원1',
-cellphoneNo = '01012341234',
-email = 'admin@sbs.com';
-
+cellphoneNo = '01011111111',
+email = 'admin@gmail.com';
 
 INSERT INTO `member`
 SET regDate = NOW(),
 updateDate = NOW(),
-loginId = 'member2',
-loginPw = 'member2',
-authLevel = 3,
-`name` = '회원2',
-nickname = '회원2',
-cellphoneNo = '01012341234',
-email = 'admin@sbs.com';
+loginId = 'user1',
+loginPw = 'user1',
+`name` = '사용자1',
+nickname = '사용자1',
+cellphoneNo = '01011111111',
+email = 'user1@gmail.com';
 
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '사용자2',
+nickname = '사용자2',
+cellphoneNo = '01011111111',
+email = 'user2@gmail.com';
 
+SELECT * FROM `member`;
 
-SELECT * FROM MEMBER;
-
-
-
-#게시판 테이블 생성
-CREATE TABLE board(
+# 게시판 테이블 생성
+CREATE TABLE board (
     id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
     updateDate DATETIME NOT NULL,
-    `code` CHAR(50) NOT NULL COMMENT 'notice(공지사항), free(자유게시판1), free(자유게시판2,...',
-    `name` CHAR(50) NOT NULL COMMENT '게시판 이름',
+    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항), free1(자유게시판1), free2(자유게시판2,...',
+    `name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
     delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=탈퇴전, 1=탈퇴)',
     delDate DATETIME COMMENT '삭제날짜'
 );
 
-#기본 게시판 생성
+# 기본 게시판 생성
 INSERT INTO board
 SET regDate = NOW(),
 updateDate = NOW(),
-`code`= 'notice',
-`name`= '공지사항';
+`code` = 'notice',
+`name` = '공지사항';
 
 INSERT INTO board
 SET regDate = NOW(),
 updateDate = NOW(),
-`code`= 'free1',
-`name`= '자유';
+`code` = 'free1',
+`name` = '자유';
 
-ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+
 SELECT * FROM article;
 
+# 게시판 테이블에 boardId 컬럼 추가
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
+
+# 1, 2번 게시물을 공지사항 게시물로 지정
 UPDATE article
-SET boardId=1
-WHERE boardId = 0;
+SET boardId = 1
+WHERE id IN(1,2);
 
+# 3번 게시물을 자유게시판 게시물로 지정
 UPDATE article
-SET boardId=2
-WHERE id IN(12,17,19);
+SET boardId = 2
+WHERE id IN(3);
 
+SELECT * FROM board WHERE id = 1;
+SELECT * FROM board WHERE id = 2;
 
-#게시물 개수 늘리기
+/*
+# 게시물 개수 늘리기
 INSERT INTO article
 (
-    regDate,updateDate,memberId,boardId,title,`body`
+    regDate, updateDate, memberId, boardId, title, `body`
 )
-SELECT NOW(),NOW(),FLOOR(RAND()*2)+1,FLOOR(RAND()*2)+1,CONCAT('제목_',RAND()),CONCAT('내용_',RAND())
-FROM article;
+select NOW(), now(), FLOOR(RAND() *2) + 1, FLOOR(RAND() *2) + 1, concat('제목_', rand()), CONCAT('내용_', RAND())
+from article;
+*/
+
+SELECT * FROM article;
+
+DESC article;
+
+ALTER TABLE article
+ADD COLUMN hitCount INT(10) UNSIGNED NOT NULL DEFAULT 0;
+
+# like 테이블 생성
+CREATE TABLE reactionPoint (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    memberId INT(10) UNSIGNED NOT NULL,
+    relTypeCode CHAR(30) NOT NULL COMMENT '관련데이터타입코드',
+    relId INT(10) UNSIGNED NOT NULL COMMENT '관련데이터번호',
+    `point` SMALLINT(2) NOT NULL
+);
+
+# 리액션포인트 테스트 데이터
+# 1번 회원이 1번 article에 대해서 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+reltypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 1번 회원이 2번 article에 대해서 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+reltypeCode = 'article',
+relId = 2,
+`point` = 1;
+
+# 2번 회원이 1번 article에 대해서 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+reltypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 2번 회원이 2번 article에 대해서 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+reltypeCode = 'article',
+relId = 2,
+`point` = 1;
+
+# 3번 회원이 1번 article에 대해서 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 3,
+reltypeCode = 'article',
+relId = 1,
+`point` = 1;
+
+SELECT * FROM reactionPoint;
