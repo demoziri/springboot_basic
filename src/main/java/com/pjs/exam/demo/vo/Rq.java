b49a491778d0efc58802b572ffb0e8972f8ba32b
demoziri <redhorangy1@naver.com>
2023-05-02 15:47:15 +0900

    로그인 상태에서 작성버튼 누르면 로그인 페이지로 이동

--- src/main/java/com/pjs/exam/demo/vo/Rq.java
+++ src/main/java/com/pjs/exam/demo/vo/Rq.java
@@ -112,6 +112,12 @@ public class Rq {
 
 
 	public void initOnBeforeActionInterceptor() {}
+
+	public void printReplaceJs(String msg, String uri) {
+		resp.setContentType("text/html; charset=UTF-8");
+		print(Ut.jsReplace(msg,uri));
+		
+	}
 	
 	
 }
